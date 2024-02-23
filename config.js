03650e58956f0ff8df5a0b272a32238df89a8dcf
elsayed kamal <sayed.kamal@waseet.net>
2024-02-23 11:28:49 +0200

    “wip”

---
+++ config.js
@@ -0,0 +1,21 @@
+window.__CONFIG__ = {
+  // The URL for the CORS proxy, the URL must NOT end with a slash!
+  VITE_CORS_PROXY_URL: "https://65d859f68e8d87c06649fbb2--bejewelled-beignet-7aa442.netlify.app",
+
+  // The READ API key to access TMDB
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OWJlMDJjYjM4ZDdkMmQxZjQzMjJmZDQwZDE1MDRmYSIsInN1YiI6IjYzOWEwMjBjYTRhZjhmMDA5MWNkYjcxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p_FC4z9jpHDnL2K-xLDlRMXMoAYs_M-FE8twYmk47UI",
+
+  // The DMCA email displayed in the footer, null to hide the DMCA link
+  VITE_DMCA_EMAIL: null,
+
+  // Whether to disable hash-based routing, leave this as false if you don't know what this is
+  VITE_NORMAL_ROUTER: false,
+
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
+  VITE_BACKEND_URL: "http://69.48.186.32:1245",
+
+  // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
+  VITE_DISALLOWED_IDS: ""
+};
+
+
